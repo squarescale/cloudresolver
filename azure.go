a4507dc0e74958a004e8ea386a9010120e03a922
Olivier Bourdon <olivier.bourdon@squarescale.com>
2020-06-29 17:35:14 +0200

    Start to add Azure support

---
+++ azure.go
@@ -0,0 +1,240 @@
+package cloudresolver
+
+import (
+	"context"
+	"fmt"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-12-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-05-01/network"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/davecgh/go-spew/spew"
+	"os"
+	"strings"
+)
+
+type AzureResolver struct{}
+
+type IpInfos struct {
+	PrivateIpv4  string
+	PrivateIpv6  string
+	PrivateName  string
+	PublicIpv4   string
+	PublicIpv6   string
+	PublicName   string
+}
+
+func init() {
+	register("azure", new(AzureResolver))
+}
+
+func getAuthorizer() (autorest.Authorizer, error) {
+	//authorizer, err := auth.NewAuthorizerFromCLI()
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		return nil, err
+	}
+	return authorizer, nil
+}
+
+func getVMClient() (*compute.VirtualMachinesClient, error) {
+	vmClient := compute.NewVirtualMachinesClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	authorizer, err := getAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	vmClient.Authorizer = authorizer
+	return &vmClient, nil
+}
+
+// GetAllVMs gets all VMs info
+func GetAllVMs(ctx context.Context) (*compute.VirtualMachineListResultIterator, error) {
+	vmClient, err := getVMClient()
+	if err != nil {
+		return nil, err
+	}
+	res, ret1 := vmClient.ListAllComplete(ctx, "false")
+	return &res, ret1
+}
+
+func getVMSSClient() (*compute.VirtualMachineScaleSetsClient, error) {
+	vmSSClient := compute.NewVirtualMachineScaleSetsClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	authorizer, err := getAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	vmSSClient.Authorizer = authorizer
+	return &vmSSClient, nil
+}
+
+// GetAllVMScaleSets gets all VM Scale Sets info
+func GetAllVMSSs(ctx context.Context) (*compute.VirtualMachineScaleSetListWithLinkResultIterator, error) {
+	vmSSClient, err := getVMSSClient()
+	if err != nil {
+		return nil, err
+	}
+	res, ret1 := vmSSClient.ListAllComplete(ctx)
+	return &res, ret1
+}
+
+func getNetITFClient() (*network.InterfacesClient, error) {
+	netITFClient := network.NewInterfacesClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	authorizer, err := getAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	netITFClient.Authorizer = authorizer
+	return &netITFClient, nil
+}
+
+func getPubIPCfgClient() (*network.PublicIPAddressesClient, error) {
+	pubIPCfgClient := network.NewPublicIPAddressesClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	authorizer, err := getAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	pubIPCfgClient.Authorizer = authorizer
+	return &pubIPCfgClient, nil
+}
+
+func getNetIPCfgClient() (*network.InterfaceIPConfigurationsClient, error) {
+	netIPCfgClient := network.NewInterfaceIPConfigurationsClient(os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	authorizer, err := getAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	netIPCfgClient.Authorizer = authorizer
+	return &netIPCfgClient, nil
+}
+
+// GetNetworkInterfaceInfos gets infos on specific network interface
+func GetNetworkInterfaceInfos(ctx context.Context, reference compute.NetworkInterfaceReference) (*IpInfos, error) {
+	ipi := IpInfos{}
+	netITFClient, err := getNetITFClient()
+	if err != nil {
+		return nil, err
+	}
+	/*netIPCfgClient, err := getNetIPCfgClient()
+	if err != nil {
+		return nil, err
+	}*/
+	pubIPCfgClient, err := getPubIPCfgClient()
+	if err != nil {
+		return nil, err
+	}
+	netITFID := strings.Split(*reference.ID, "/")
+	res, err := netITFClient.Get(ctx, netITFID[4], netITFID[8], "")
+	if err != nil {
+		return nil, err
+	}
+	for _, ipcfg := range *res.IPConfigurations {
+		//fmt.Printf("x: %s\n", spew.Sdump(ipcfg))
+		pubITFID := strings.Split(*ipcfg.PublicIPAddress.ID, "/")
+		if *ipcfg.Primary && ipcfg.PrivateIPAddressVersion == network.IPv4 {
+			ipi.PrivateIpv4 = *ipcfg.PrivateIPAddress
+		}
+		if *ipcfg.Primary && ipcfg.PrivateIPAddressVersion == network.IPv6 {
+			ipi.PrivateIpv6 = *ipcfg.PrivateIPAddress
+		}
+		pubipcfg, err := pubIPCfgClient.Get(ctx, pubITFID[4], pubITFID[8], "")
+		if err != nil {
+			return nil, err
+		}
+		if pubipcfg.PublicIPAddressVersion == network.IPv4 {
+			ipi.PublicIpv4 = *pubipcfg.IPAddress
+		}
+		if pubipcfg.PublicIPAddressVersion == network.IPv6 {
+			ipi.PublicIpv6 = *pubipcfg.IPAddress
+		}
+		if pubipcfg.DNSSettings != nil {
+			ipi.PublicName = *pubipcfg.DNSSettings.Fqdn
+		}
+		//fmt.Printf("x: %s\n", spew.Sdump(pubipcfg.PublicIPAddressPropertiesFormat))
+	}
+	return &ipi, nil
+}
+
+func (r AzureResolver) Resolve(name string, config map[string]interface{}) ([]Host, error) {
+	//ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	//defer cancel()  // releases resources if slowOperation completes before timeout elapses
+	allVMs, err := GetAllVMs(context.Background())
+	if err != nil {
+		return []Host{}, err
+	}
+	hosts := []Host{}
+	for subList := allVMs; subList.NotDone(); err = subList.NextWithContext(context.Background()) {
+		vm := subList.Value()
+		//fmt.Printf("VM: %s", spew.Sdump(vm))
+		tags := make(map[string]string)
+		iname := ""
+		for k, v := range vm.Tags {
+			if k == "Name" {
+				iname = *v
+			}
+			tags[k] = *v
+		}
+		if len(name) > 0 {
+			if name != "*" && !strings.HasPrefix(iname, name) {
+				continue
+			}
+		}
+		h := Host{
+			InstanceName: iname,
+			MachineType:  string(vm.VirtualMachineProperties.HardwareProfile.VMSize),
+			Provider:     "azure",
+			Region:       *vm.Location,
+			//Zone:         *inst.Placement.AvailabilityZone,
+			Id:           *vm.ID,
+			Tags:         tags,
+		}
+		for _, ni := range *vm.NetworkProfile.NetworkInterfaces {
+			niInfos, err := GetNetworkInterfaceInfos(context.Background(), ni)
+			if err != nil {
+				fmt.Printf("IP error: %s\n", spew.Sdump(err))
+				return []Host{}, err
+			}
+			h.PrivateIpv4 = niInfos.PrivateIpv4
+			h.Private     = niInfos.PrivateIpv4
+			h.PublicIpv4  = niInfos.PublicIpv4
+			h.Public      = niInfos.PublicIpv4
+			h.PublicName  = niInfos.PublicName
+		}
+		hosts = append(hosts, h)
+	}
+	allVMSSs, err := GetAllVMSSs(context.Background())
+	if err != nil {
+		return []Host{}, err
+	}
+	//fmt.Printf("x: %s\n", spew.Sdump(y))
+	//fmt.Printf("err: %s\n", spew.Sdump(err))
+	for subList := allVMSSs; subList.NotDone(); err = subList.NextWithContext(context.Background()) {
+		vmss := subList.Value()
+		fmt.Printf("x: %s\n", spew.Sdump(vmss))
+		tags := make(map[string]string)
+		iname := ""
+		for k, v := range vmss.Tags {
+			if k == "Name" {
+				iname = *v
+			}
+			tags[k] = *v
+		}
+		if vmss.VirtualMachineProfile != nil && vmss.VirtualMachineProfile.OsProfile != nil && vmss.VirtualMachineProfile.OsProfile.ComputerNamePrefix != nil {
+			iname = *vmss.VirtualMachineProfile.OsProfile.ComputerNamePrefix
+		}
+		if len(name) > 0 {
+			if name != "*" && !strings.HasPrefix(iname, name) && !strings.HasPrefix(*vmss.Name, name){
+				continue
+			}
+		}
+		h := Host{
+			InstanceName: iname,
+			MachineType:  *vmss.Sku.Name,
+			Provider:     "azure",
+			Region:       *vmss.Location,
+			//Zone:         *inst.Placement.AvailabilityZone,
+			Id:           *vmss.ID,
+			Tags:         tags,
+		}
+		hosts = append(hosts, h)
+	}
+	return hosts, nil
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,13 @@ go 1.14
 
 require (
 	cloud.google.com/go v0.58.0 // indirect
+	github.com/Azure/azure-sdk-for-go v43.2.0+incompatible
+	github.com/Azure/go-autorest/autorest v0.10.2 // indirect
+	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
 	github.com/aws/aws-sdk-go v1.32.1
+	github.com/davecgh/go-spew v1.1.1
 	github.com/digitalocean/godo v1.37.0
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
